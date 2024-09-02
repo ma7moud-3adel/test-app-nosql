6a468a1a825afb4a6dbf4f17670ca1ed1b5926f9
Mahmoud Adel <34139121+ma7moud-3adel@users.noreply.github.com>
2024-09-02 15:01:28 +0300

    Operation Get All by mongooes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,10 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  // *-----------------*
+  // @desk : Get All Users From DB
+  // @route : Get
+  // @access : Puplic
   @Get()
   getAllUsers() {
     return this.userService.getAllUsers();
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,7 +8,10 @@ export class UserService {
     @Inject('USER_MODEL')
     private userModel: Model<User>,
   ) {}
-
+  // *-----------------*
+  // @desk : Get All Users From DB
+  // @route : Get
+  // @access : Puplic
   getAllUsers(): Promise<User[]> {
     return this.userModel.find();
   }
