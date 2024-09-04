ffc58c1017356857945061240056c8d3bf540cde
Mahmoud Adel <34139121+ma7moud-3adel@users.noreply.github.com>
2024-09-04 15:24:41 +0300

    hash password and compare using bcrypt

--- src/user/service/auth/auth.service.ts
+++ src/user/service/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Model } from 'mongoose';
 import { CreateUserDto } from 'src/user/dto/admin/createUser.dto';
 import { SingInDto } from 'src/user/dto/auth/signin.dto';
 import { User } from 'src/user/interfaces/user.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,14 +13,26 @@ export class AuthService {
   ) {}
   async signIn(body: SingInDto) {
     const { email, password } = body;
-    const user = await this.userModel.findOne({ email, password });
+    const user = await this.userModel.findOne({ email });
     if (!user) {
+      throw new NotFoundException();
+    }
+    const hashed = user.password;
+    const isMatch = await bcrypt.compare(password, hashed);
+    if (!isMatch) {
       throw new NotFoundException();
     }
     return user;
   }
   async signUp(body: CreateUserDto) {
-    const user = await this.userModel.create(body);
+    const password = await bcrypt.hash(body.password, 10);
+    const hashed = {
+      name: body.name,
+      age: body.age,
+      email: body.email,
+      password,
+    };
+    const user = await this.userModel.create(hashed);
     return user;
   }
 }
